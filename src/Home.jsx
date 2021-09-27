585471c1986bfc8958c2772f76a65cf7d265a2a9
Alexandre Ribeiro da Silva <ardsilva87@gmail.com>
2021-09-27 11:00:15 -0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,29 +1,46 @@
 import './App.css';
 import React from 'react';
 import logo from './logo.svg';
+import axios from "axios";
 
 class Home extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                </header>
-            </div>
-            
-        );
-    }
+    state = {
+    forceSide: {},
+    isLoading: true,
+    errors: null
+  };
+
+  getForceSide() {
+    Promise.race([axios
+      .get("https://swapi.dev/api/people/1"), axios
+      .get("https://swapi.dev/api/people/4")])
+    .then(res => {console.log(res.data);
+      this.setState({forceSide: res.data});
+    }).catch(err => console.log(err))
+    .finally(() => this.setState({isLoading: false}));
+  }
+
+  componentDidMount() {
+    this.getForceSide();
+  }
+
+  render() {
+    const { isLoading, forceSide } = this.state;
+    return (
+      <React.Fragment>
+        <div><button>back</button></div>
+        <div><button onClick={() => this.getForceSide()}>choose your path again, Padawan</button></div>
+        
+        <div>
+          {!isLoading ? (
+            <p>Your master is <strong>{forceSide.name}</strong></p>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </React.Fragment>
+    );
+  }
 
 }
 
