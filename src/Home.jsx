fb8a08d590d6e06e130597333f64134eaa4d5cce
Alexandre Ribeiro da Silva <ardsilva87@gmail.com>
2021-09-27 16:37:36 -0300

    removendo arquivos desnecessarios

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,11 +3,45 @@ import axios from 'axios';
 import { useHistory } from "react-router-dom";
 import vader from './darth-vader.png';
 import luke from './luke-skywalker.png';
+import styled, {ThemeProvider} from 'styled-components';
+import theme from 'styled-theming';
+
+const boxBackgroundColor = theme('mode', {
+  Darth: '#2A2A2A',
+  Luke: '#FBFE63',
+});
+
+const boxColor = theme('mode', {
+    Darth: '#FFFFFF',
+    Luke: '#2A2A2A',
+  });
+
+const Box = styled.div`
+  background-color: ${boxBackgroundColor};
+  color: ${boxColor};
+`;
+
+const StyledButton = styled.button`
+    background-color: ${boxColor};
+    color: ${boxBackgroundColor};
+    cursor: pointer;
+`;
+
+const BackButton  = styled.button`
+    background-color: ${boxColor};
+    color: ${boxBackgroundColor};
+    cursor: pointer;
+`;
+
+const Image = styled.img`
+    border-radius: 50%;
+`;
 
 const Home = () => {
     const history = useHistory();
     const [forceSide, setForceSide] = useState({});
     const [isLoading, setIsLoading] = useState(true);
+    const [theme, setTheme] = useState('');
 
   const getForceSide = () => {
     setIsLoading(true);
@@ -16,6 +50,7 @@ const Home = () => {
       .get("https://swapi.dev/api/people/4")])
     .then(res => {console.log(res.data);
       setForceSide(res.data);
+      setTheme(res.data.name.split(' ')[0]);
     }).catch(err => console.log(err))
     .finally(() => setIsLoading(false));
   }
@@ -25,26 +60,31 @@ const Home = () => {
   }, []);
 
     return (
-      <React.Fragment>
-        <div><button onClick={() => history.push('/force-challange')}>back</button></div>
-        <div>
-            <button disabled={isLoading} onClick={() => getForceSide()}>choose your path again, Padawan</button>
-        </div>
-        <div>
-          {!isLoading ? (
-              <>
-                <img 
-                    style={{ borderRadius: '50%' }}
-                    src={forceSide.name === 'Darth Vader' ? vader : luke} 
-                    alt="Master" 
-                />
-                <p>Your master is <strong>{forceSide.name}</strong></p>
-            </>
-          ) : (
-            <p>Checking ...</p>
-          )}
-        </div>
-      </React.Fragment>
+    <ThemeProvider theme={{ mode: theme }}>
+        <Box>
+            <div>
+                <BackButton onClick={() => history.push('/force-challange')}>back</BackButton>
+            </div>
+            <div>
+                <StyledButton
+                disabled={isLoading} 
+                onClick={() => getForceSide()}>choose your path again, Padawan</StyledButton>
+            </div>
+            <div>
+            {!isLoading ? (
+                <div>
+                    <Image 
+                        src={forceSide.name === 'Darth Vader' ? vader : luke} 
+                        alt="Master" 
+                    />
+                    <p>Your master is <strong>{forceSide.name}</strong></p>
+                </div>
+            ) : (
+                <p>Checking ...</p>
+            )}
+            </div>
+        </Box>
+      </ThemeProvider>
     );
   }
 
