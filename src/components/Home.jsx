ade870b2f9ec2e728fed9a7d51fd9aee281ed681
Alexandre Ribeiro da Silva <ardsilva87@gmail.com>
2021-09-28 01:11:04 -0300

    Separacao dos componentes, responsividade e layout melhorados

---
+++ src/components/Home.jsx
@@ -0,0 +1,166 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useHistory } from "react-router-dom";
+import vader from '../assets/darth-vader.png';
+import luke from '../assets/luke-skywalker.png';
+import styled, {ThemeProvider} from 'styled-components';
+import theme from 'styled-theming';
+
+const boxBackgroundColor = theme('mode', {
+  Darth: '#2A2A2A',
+  Luke: '#FBFE63',
+});
+
+const boxColor = theme('mode', {
+    Darth: '#FFFFFF',
+    Luke: '#2A2A2A',
+  });
+
+const Box = styled.div`
+  background-color: ${boxBackgroundColor};
+  color: ${boxColor};
+`;
+
+const StyledButton = styled.button`
+    background-color: ${boxColor};
+    color: ${boxBackgroundColor};
+    cursor: pointer;
+    font-size: 1em;
+    font-weight: bold;
+    margin: 1em;
+    padding: 1em;
+    border: 2px solid ${boxColor};
+    border-radius: 10px;
+
+    &:disabled {
+      opacity: .5;
+      cursor: wait;
+    }
+
+    @media(max-width: 799px) {
+      display: none
+    }
+`;
+
+const MobileStyledButton = styled.button`
+    background-color: ${boxColor};
+    color: ${boxBackgroundColor};
+    cursor: pointer;
+    font-size: 1em;
+    font-weight: bold;
+    margin: 1em;
+    padding: 1em;
+    border: 2px solid ${boxColor};
+    border-radius: 10px;
+
+    &:disabled {
+      opacity: .5;
+      cursor: wait;
+    }
+
+    @media(min-width: 800px) {
+      display: none
+    }
+`;
+
+const BackButton  = styled.button`
+    cursor: pointer;
+    background-color: ${boxBackgroundColor};
+    border: none;
+    display: grid;
+`;
+
+const Icon = styled.svg`
+    fill: currentColor;
+    color: ${boxColor};
+`;
+
+const Label = styled.span`
+    color: ${boxColor};
+    font-size: 18px;
+    line-height: 22px;
+`;
+
+const Image = styled.img`
+    border-radius: 50%;
+    width: 20%;
+    @media(max-width: 768px) {
+      width: 50%;
+  }
+`;
+
+const P = styled.p`
+    font-style: normal;
+    font-weight: normal;
+    font-size: 36px;
+    line-height: 44px;
+`;
+
+const Strong = styled.strong`
+  @media(max-width: 768px) {
+    display: list-item;
+  }
+`;
+
+const Home = () => {
+    const history = useHistory();
+    const [forceSide, setForceSide] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [theme, setTheme] = useState('');
+
+  const getForceSide = () => {
+    setIsLoading(true);
+    Promise.race([
+      axios.get("https://swapi.dev/api/people/1"),
+      axios.get("https://swapi.dev/api/people/4")
+    ]).then(res => {console.log(res.data);
+      setForceSide(res.data);
+      setTheme(res.data.name.split(' ')[0]);
+    }).catch(err => console.log(err))
+    .finally(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    getForceSide();
+  }, []);
+
+    return (
+    <ThemeProvider theme={{ mode: theme }}>
+        <Box>
+            <div>
+                <BackButton onClick={() => history.push('/force-challange')}>
+                  <Icon viewBox="0 0 32 32">
+                    <path d="M26.025 14.496l-14.286-.001 6.366-6.366L15.979 6 5.975 16.003 15.971 26l2.129-2.129-6.367-6.366h14.29z"/>
+                  </Icon>
+                  <Label>back</Label>
+                </BackButton>
+            </div>
+            <div>
+            <StyledButton
+                disabled={isLoading} 
+                onClick={() => getForceSide()}>choose your path again, Padawan
+            </StyledButton>
+            {!isLoading ? (
+                <div>
+                    <Image 
+                        src={forceSide.name === 'Darth Vader' ? vader : luke} 
+                        alt="Master" 
+                    />
+                    <P>Your master is <Strong>{forceSide.name}</Strong></P>
+                </div>
+            ) : (
+              <div>
+                <P>Checking ...</P>
+              </div>
+            )}
+            <MobileStyledButton
+                disabled={isLoading} 
+                onClick={() => getForceSide()}>choose your path again, Padawan
+            </MobileStyledButton>
+            </div>
+        </Box>
+      </ThemeProvider>
+    );
+  }
+
+export default Home;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useHistory } from "react-router-dom";
+import styled from 'styled-components';
+
+const StartButton = styled.button`
+    font-size: 1em;
+    margin: 1em;
+    padding: 1em 1.25em;
+    border: 2px solid #2C97D1;
+    border-radius: 10px;
+    background: #2C97D1;
+    color: #FFFFFF;
+    letter-spacing: 0.35em;
+`;
+
+const Title = styled.h2`
+    font-size: 72px;
+    font-weight: 100;
+    color: #2C97D1;
+`;
+
+const SubTitle = styled.h3`
+    color: #2C97D1;
+    font-size: 14px;
+    font-weight: 400;
+    margin: -60px 0px 60px 0px;
+    letter-spacing: 0.35em;
+`;
+
+const Welcome = () => {
+    const history = useHistory();
+    return(
+        <div>
+            <Title>Welcome to <strong>iClinic</strong></Title>
+            <SubTitle>FRONTEND CHALLENGE</SubTitle>
+            <StartButton onClick={() => history.push('/side-pick')}>START</StartButton>
+        </div>
+    )
+}
+
+export default Welcome;
